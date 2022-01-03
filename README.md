bf193fb1bcc9e32f01a1aba24194ca5c5059d9af
Shivam Gupta <shivam.gupta@deskera.com>
2022-01-03 14:57:08 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # employee-service
+
+###Start the database using docker command
+
+`docker run --rm --name employee_db -d -p 5433:5432 -e POSTGRES_USER=employee -e POSTGRES_PASSWORD=employee -e POSTGRES_DB=employee_db postgres:9.6`
---
+++ src/test/java/com/shivam/employeeservice/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.employeeservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
